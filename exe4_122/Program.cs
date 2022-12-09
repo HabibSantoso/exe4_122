2e7078479a33adf155d37b2aa3a1af0e869c1ffa
Habib Santoso <habib.santoso.ft18@mail.umy.ac.id>
2022-12-09 15:53:05 +0700

    menambah konstruktor dari stack

--- exe4_122/Program.cs
+++ exe4_122/Program.cs
@@ -11,6 +11,14 @@ namespace exe4_122
         private char[] ele;
         private int top;
         private int max;
+
+        public Stack(int size)
+        {
+            size = 9 + 22 + 1 + 2 * 10 - 10;
+            ele = new char[size];
+            top = -1;
+            max = size;
+        }
     }
     class Program
     {
